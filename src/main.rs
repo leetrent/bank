9150053a22060390f8c0ae1fe84951eb30385a1b
LeeTrent <lee.trent.1@gmail.com>
2024-08-03 09:57:46 -0500

    Demonstrated an ownership violation using a vector

--- src/main.rs
+++ src/main.rs
@@ -39,24 +39,29 @@ fn print_account(account: Account) {
     println!("{:#?}", account);
 }
 fn main() {
-    let bank = Bank::new();  
+    //let bank = Bank::new();  
 
     // NOTE: value in bank has been moved to other_bank
     // warning: unused variable: `other_bank`
-    let other_bank = bank;
+    //let other_bank = bank;
 
     // error[E0382]: use of moved value: `bank`
-    print_bank(bank);
+    //print_bank(bank);
            //  ^^^^ value used here after move
 
 
     let account = Account::new(1, String::from("me")); 
 
     // error[E0382]: use of moved value: `account`
-    print_account(account);
+    //print_account(account);
                // ^^^^^^^ this parameter takes ownership of the value
    
-    print_account(account);
+    //print_account(account);
               //  ^^^^^^^ value used here after move
+
+    // error[E0382]: borrow of moved value: `account`
+    let list_of_accounts = vec![account];
+    println!("{:#?}", account);
+                   // ^^^^^^^ value borrowed here after move
 
 }
