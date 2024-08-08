80626df668639c5e204738aabd293cee42d46be8
LeeTrent <lee.trent.1@gmail.com>
2024-08-07 21:04:41 -0500

    Done with immutable references

--- src/main.rs
+++ src/main.rs
@@ -56,26 +56,34 @@ fn main() {
     //////////////////////////////////////////////////////////////////
     // Scenario #2:
     /////////////////////////////////////////////////////////////////
-    //let account = Account::new(1, String::from("me")); 
-    //print_account(account); // ------- value moved here
-    //print_account(account); // error[E0382]: use of moved value: `account`
-    //            ^^^^^^^ value used here after move
+    let account = Account::new(1, String::from("me"));
+    print_account(&account_ref);
+    println!("{:#?}", account);
  
     //////////////////////////////////////////////////////////////////
     // Scenario #3:
     /////////////////////////////////////////////////////////////////
-    //let account = Account::new(1, String::from("me")); 
-    //let list_of_accounts = vec![account]; // ------- value moved here
-    //println!("{:#?}", account); // error[E0382]: borrow of moved value: `account`
-    //                ^^^^^^^ value borrowed here after move
+    let account = Account::new(1, String::from("me"));
+    let account_ref_1 = &account;
+    let account_ref_2 = &account;
+
+    print_account(account_ref_1);
+    print_account(account_ref_2);
+    println!("{:#?}", account);
 
     //////////////////////////////////////////////////////////////////
     // Scenario #4:
     /////////////////////////////////////////////////////////////////
-    //let bank = Bank::new();
-    //let accounts = bank.accounts; // ------------- value moved here
-    //println!("{:#?}", bank.accounts); // error[E0382]: borrow of moved value: `bank.accounts`
-    //                ^^^^^^^^^^^^^ value borrowed here after move
+    let account = Account::new(1, String::from("me"));
+    let account_ref_1 = &account;
+    let account_ref_2 = &account;
+    let other_account = account; // error[E0505]: cannot move out of `account` because it is borrowed
+    //                           ^^^^^^^ move out of `account` occurs here
+
+    print_account(account_ref_1);
+    print_account(account_ref_2);
+    println!("{:#?}", account); // error[E0382]: borrow of moved value: `account`
+    //                ^^^^^^^ value borrowed here after move
 
     //////////////////////////////////////////////////////////////////
     // Scenario #5:
