4765d37030f1eb5db56e748d8f352aee97ea4640
leetrent <10787590+leetrent@users.noreply.github.com>
2024-08-17 10:17:28 -0500

    Bank implementation (#1)
    * Added add_account method to bank
    
    * Added deposit and withdraw method to Account
    
    * Added summary method to Account
    
    * Completed project

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,20 @@ impl Account {
             balance: 0,
         }
     }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance of {}", self.holder, self.balance)
+    }
 }
 
 #[derive(Debug)]
@@ -29,19 +43,49 @@ impl Bank {
         // Implicit return (no 'return' statement and no semicolon at the end of the implicit return statement)
         Bank { accounts: vec![] /*empty vector*/ }
     }
+
+    // Method
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+
+    }
+
+    // Method
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    // Method
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
-fn print_bank(bank: Bank) {
-    println!("{:#?}", bank);
-}
-
-fn print_account(account: Account) {
-    println!("{:#?}", account);
-}
 fn main() {
-    let bank = Bank::new();
-    print_bank(bank);
+    let mut bank = Bank::new();
 
-    let account = Account::new(1, String::from("Casey"));
-    print_account(account);
+    //////////////////////////////////////////////////////////////////////////////////
+    // Casey's account
+    //////////////////////////////////////////////////////////////////////////////////
+    let mut account_casey = Account::new(1, String::from("Casey"));  
+    account_casey.deposit(1000);
+    account_casey.withdraw(250);
+    bank.add_account(account_casey);
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Pooh's account
+    //////////////////////////////////////////////////////////////////////////////////
+    let mut account_pooh = Account::new(1, String::from("Pooh Bear"));  
+    account_pooh.deposit(500);
+    account_pooh.withdraw(250);
+    bank.add_account(account_pooh);
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Bank summary of accounts
+    //////////////////////////////////////////////////////////////////////////////////
+    println!("{:#?}", bank.summary());
+    println!("Total of all account balances: {}", bank.total_balance());
 }
